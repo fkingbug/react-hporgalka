e1ff50f46071fb856a7ed892960544e2594466ca
fkingbug <fkingbug@gmail.com>
2021-08-30 20:03:21 +0300

    fist

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import PostForm from './components/PostForm'
 
 import './styles/App.css'
 import PostFilter from './components/PostFilter'
+import MyModal from './components/UI/MyModal/MyModal'
+import MyButton from './components/UI/button/MyButton'
 
 function App() {
   const [posts, setPosts] = useState([
@@ -11,7 +13,9 @@ function App() {
     { id: 3, title: 'bb', body: 'bb' },
     { id: 2, title: 'cc', body: 'aa' },
   ])
+
   const [filter, setFilter] = useState({ sort: '', query: '' })
+  const [modal, setModal] = useState(false)
 
   const sortedPosts = useMemo(() => {
     console.log('Мемо сработало')
@@ -22,11 +26,12 @@ function App() {
   }, [filter.sort, posts])
 
   const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
   }, [filter.query, sortedPosts])
 
   const createPost = newPost => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
 
   const removePost = post => {
@@ -35,14 +40,16 @@ function App() {
 
   return (
     <div className="App">
-      <PostForm create={createPost} />
+      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+        Создать пост
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
+
       <hr style={{ margin: '15px 0' }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostList
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title="Список постов 1"
-      />
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов 1" />
     </div>
   )
 }
